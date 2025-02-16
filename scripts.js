5b8c44d06d6e5c6dee9e011e5724883fd1915530
Payton <paytonl@duck.com>
2025-02-15 22:39:14 -0700

    added random color generation based on date

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 /*
 TODO:
 make default colors for certain dates (i.e. pink for valentine's day, green for st. patty's day etc.)
+make text always stand out
 */
 
 const genDateString = () => {
@@ -12,18 +13,21 @@ const genDateString = () => {
 };
 
 const randomColorFromDate = () => {
-  const colorSeed = genDateString();
-
-  /*
-  declare hexString as "#"
-  get two random numbers from colorSeed
-  redPercent = divide those numbers by 31
-  make sure redPercent is between 0 and 1
-  redNumber = multiply redPercent by 256
-  convert redNumber to hexCode
-  add redNumber to hexString
-  repeat for green and blue
-  */
+  let colorSeed = genDateString();
+  colorSeed = (colorSeed * 1664525 + 1013904223) | 0;
+  colorSeed = colorSeed >>> 0;
+  while (colorSeed.toString().length < 10) {
+    colorSeed *= 10;
+  }
+  const redDec = parseInt(colorSeed.toString().substring(0, 3));
+  const greenDec = parseInt(colorSeed.toString().substring(3, 6));
+  const blueDec = parseInt(colorSeed.toString().substring(6, 9));
+  const redHex = Math.trunc((redDec / 999) * 255).toString(16);
+  const greenHex = Math.trunc((greenDec / 999) * 255).toString(16);
+  const blueHex = Math.trunc((blueDec / 999) * 255).toString(16);
+  const result = "#" + redHex + greenHex + blueHex;
+  return result;
 };
 
-console.log("Hello World"[4]);
+const body = document.querySelector("body");
+body.style.backgroundColor = randomColorFromDate();
